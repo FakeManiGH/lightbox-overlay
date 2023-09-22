d93494a52decff7f3ff97d695b49a53841d9ba1f
Timo Anjala <98460302+FakeManiGH@users.noreply.github.com>
2023-09-22 14:50:05 +0300

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,114 @@
+*******
+* LBO *
+*******
+
+Lightbox Overlay - LBO
+(c) Timo Anjala
+Version: 1.12
+
+
+1. What is Lightbox Overlay (LBO)?
+2. How to setup Lightbox Overlay?
+3. Using Lightbox Overlay.
+4. Using Download function.
+5. Browser support.
+6. Known bugs.
+7. More information.
+
+
+
+
+1. WHAT IS LIGHTBOX OVERLAY?	
+----------------------------
+	Overlay (LBO) is JavaScript “WIDGET" that creates an independent image viewing layer with user-interface on any web-document. 
+	LBO helps to create easy to use image galleries with brwosing function, fullscreen view, and image download on any web-document. 
+	LBO is fully automated script and very easy to implement to your project. You need only basic understanding of HTML. 
+
+
+
+2. HOW TO SETUP LIGHTBOX OVERLAY?	
+---------------------------------
+	1. Setup Lightbox Overlay by downloading latest release of 'lightbox_overlay.css' and 'lightbox_overlay.js' files from GitHub.
+	
+	2. Place both files on your webserver in htdocs or specific folder where your website files are located. You can also create subfolder.
+	
+	3. Link both files to HTML document, where you wish to use lightbox Overlay for your images:
+	
+		- Add link 'lightbox_overlay.css' file anywhere in <head> section of HTML document. Example: 
+		  
+			<link rel="stylesheet" href="location/of/lightbox_overlay.css.css">
+		  
+		- Add link to 'lightbox_overlay.js' file to END of <body> section of HTML document. Example:	
+	  	
+			<script src="location/of/lightbox_overlay.js"></script>
+	  	
+	4. To activate lightbox in HTML, simply add 'lightbox-data' attribute on any image element you wish to view in Lightbox Overlay.
+		
+		- This is an example image code (myImages is example gallery name):
+			
+			<img src="location/of/image.jpg" lightbox-data="myImages" alt="description of my image">
+	
+	5. Enjoy The Overlay.
+
+
+
+3. USING LIGHTBOX OVERLAY	
+-------------------------
+	- When image on your HTML document has 'lightbox-data' -attribute, it works as link to open image in Lightbox Overlay.
+
+	- Adding the same 'lightbox-data' attribute value (eg. "myImages") menthioned in setup, allocates Lightbox Overlay gallery.
+		> Script will collect all images with same value and creates browsable gallery.
+		> Page can include many galleries with different names, script is dynamic.
+
+	- ALT-attribute from your image element will be printed in Lightbox Overlay as title of the image.
+
+	- You can move backwards and forward inside Lightbox Overlay by using arrow-buttons on screen or arrow-buttons on your keyboard.
+
+	- By clicking the image inside Lightbox Overlay, image will zoom-in to 125% size. Second click will return normal size.
+
+	- By clicking 'fullscreen' -button, you can view and browse images in device fullscreen.
+		> In fullscreen mode, browsing works with arrow buttons on your keyboard and swipes on mobile screen.
+
+	- You can close lightbox overlay by using close-button, 'BACKSPACE'-button on keyboard, or 'ESC'-button on keyboard. 
+
+
+
+4. USING DOWNLOAD FUNCTION	
+--------------------------
+	- You can allocate download button for any image you add to Lightbox Overlay. You need to add 'download' attribute to image
+	  element.
+
+		eg. <img src="location/of/image.jpg" download lightbox-data="myImages" alt="description of my image">
+		eg. <img src="location/of/image.jpg" lightbox-data="myImages" download alt="description of my image">
+		eg. <img src="location/of/image.jpg" lightbox-data="myImages" alt="description of my image" download>
+
+
+
+5. BROWSER SUPPORT
+------------------
+	Lightbox Overlay works on following popular browsers:
+
+	- Mozilla Firefox
+	- Google Chrome
+	- Opera
+	- Safari
+	- Microsoft Edge
+	- Samsung Internet (Mobile)
+
+
+
+6. KNOWN BUGS
+-------------
+	- On mobile devices, too fast browsing might skip images.
+	- On some screen sizes, image zoom leaves part of image (left) unscrollable.
+		> Fullscreen function is not efected.
+
+	!!! NOTE: If you find solution to fix these problems, please contact me: timo.anjala@gmail.com. Anyone can become contributor.
+
+
+
+7. MORE INFORMATION
+-------------------
+	More information about Lightbox Overlay at:
+
+	https://timoanjala.com/projects/lightbox.html
---
+++ src/lightbox_overlay.js
@@ -0,0 +1,380 @@
+// Lightbox Overlay (LBO) v.1.12
+// Original script was written by (c) Timo Anjala.
+// LBO is free to use in any personal or commercial projects, free of charge.
+// LBO script can be modified to fit the needs of person obtaining a copy of the it.
+
+/*
+NOTE:   Creator Informaiton and version must remain in all copies of the script. Any user-facing notification on website is not required.
+        If code is modified but parts of the orginal script remains in use, mentioning of the original script is expected.
+*/
+
+
+
+// LIGHTBOX ELEMENTS
+
+// Close SVG icon.
+const closeIcon = 
+`<svg class="lightbox-close-icon" width="1em" height="1em" viewBox="0 0 250 250" fill="currentColor">
+<path d="m125,0C55.96,0,0,55.96,0,125s55.96,125,125,125,125-55.96,125-125S194.04,0,125,0Zm78.84,180.51l-23.33,23.34-55.5-55.51-55.5,55.51-23.33-23.34,55.5-55.51-55.5-55.51,23.33-23.34,55.5,55.51,55.5-55.51,23.33,23.34-55.5,55.51,55.5,55.51Z"/>
+</svg>`;
+
+
+// Next SVG icon.
+const nextIcon = 
+`<svg class="lightbox-next-icon" width="1em" height="1em" viewBox="0 0 250 250" fill="currentColor">
+<path d="m125,0C55.96,0,0,55.96,0,125s55.96,125,125,125,125-55.96,125-125S194.04,0,125,0Zm17.5,162.5l-62.5,37.5V50l62.5,37.5,62.5,37.5-62.5,37.5Z"/>
+</svg>`;
+
+
+// previous SVG icon.
+const prevIcon = 
+`<svg class="lightbox-previous-icon" width="1em" height="1em" viewBox="0 0 250 250" fill="currentColor">
+<path d="m125,0C55.96,0,0,55.96,0,125s55.96,125,125,125,125-55.96,125-125S194.04,0,125,0Zm45,127.05v75l-62.5-37.5-62.5-37.5,62.5-37.5,62.5-37.5v75Z"/>
+</svg>`;
+
+
+// Download SVG icon.
+const downloadIcon =
+`<svg class="lightbox-download-icon" width="1em" height="1em" viewBox="0 0 250 250" fill="currentColor">
+<path d="m125,0C55.96,0,0,55.96,0,125s55.96,125,125,125,125-55.96,125-125S194.04,0,125,0Zm75,166.02v30H50v-55h30v25h45l-25-25-25-25h35V46.02h30v70h35l-25,25-25,25h45v-25h30v25Z"/>
+</svg>`;
+
+
+// Fullscreen SVG icon.
+const fullscreenIcon =
+`<svg class="lightbox-download-icon" width="1em" height="1em" viewBox="0 0 250 250" fill="currentColor">
+<path d="m125,0C55.96,0,0,55.96,0,125s55.96,125,125,125,125-55.96,125-125S194.04,0,125,0Zm80,134.29v70.71s-70.71,0-70.71,0l24.76-24.76-34.04-34.04-34.05,34.05,24.75,24.75H45s0-70.71,0-70.71l24.75,24.75,34.05-34.05-34.04-34.04-24.76,24.76V45h70.71s-24.74,24.74-24.74,24.74l34.04,34.04,34.03-34.03-24.75-24.75h70.71v70.71s-24.75-24.75-24.75-24.75l-34.03,34.03,34.04,34.04,24.74-24.74Z"/>
+</svg>`;
+
+// Creates DIV element on document to work ligthbox gallery overlay.
+const lightBox = document.createElement('div');
+lightBox.className = 'lightbox';
+document.body.appendChild(lightBox);
+
+
+// Creates container which will hold image and image-data to center of lightbox overlay.
+const imageContainer = document.createElement('div');
+imageContainer.className = 'image-container';
+lightBox.appendChild(imageContainer);
+
+
+// Flex-container to keep all elements above Lightbox-image inline.
+const infoBar = document.createElement('div');
+infoBar.className = 'info-bar';
+imageContainer.appendChild(infoBar);
+
+
+// Image element which will display the clicked image inside the lightbox.
+var lightboxImg = document.createElement('img');
+lightboxImg.className = 'lightbox-img';
+lightboxImg.setAttribute('lightbox-data', '');
+imageContainer.appendChild(lightboxImg);
+
+
+// H2 element which will display clicked images ALT-data as title in LBO.
+var imageData = document.createElement('h3');
+imageData.className = 'image-data';
+infoBar.appendChild(imageData);
+
+
+// flex-container for buttons at top right corner inside LBO.
+const infoBarBtns = document.createElement('div');
+infoBarBtns.className = 'info-bar-btns';
+infoBar.appendChild(infoBarBtns);
+
+// Creates fullscreen button on LBO.
+const fullScreen = document.createElement('a');
+fullScreen.className = 'fullscreen-btn';
+fullScreen.innerHTML = fullscreenIcon;
+fullScreen.title = 'View Fullscreen';
+infoBarBtns.appendChild(fullScreen);
+
+// btn for image download (download-icon).
+const imageDownload = document.createElement('a');
+imageDownload.className = 'image-download';
+imageDownload.innerHTML = downloadIcon;
+imageDownload.title = 'Download Image';
+infoBarBtns.appendChild(imageDownload);
+
+
+// Link Element as a close button for LBO.
+const lightClose = document.createElement('a')
+lightClose.className = 'image-close';
+lightClose.innerHTML = closeIcon;
+lightClose.title = 'Close Lightbox';
+infoBarBtns.appendChild(lightClose);
+
+
+// Flex-container to keep all elements below Lightbox-image inline.
+const slideBar = document.createElement('div');
+slideBar.className = 'slide-bar';
+imageContainer.appendChild(slideBar);
+
+
+// button to move to previus picture in gallery.
+const previousBtn = document.createElement('a');
+previousBtn.className = 'previous-btn';
+previousBtn.innerHTML = prevIcon;
+previousBtn.title = 'Previous Image';
+slideBar.appendChild(previousBtn);
+
+
+// Info slot in ligthbox to show current and total amount of pictures in gallery.
+var slideInfo = document.createElement('h3');
+slideInfo.className = 'slide-info';
+slideBar.appendChild(slideInfo);
+slideInfo.innerHTML = '';
+
+
+// button to move to next picture in gallery.
+const nextBtn = document.createElement('a');
+nextBtn.className = 'next-btn';
+nextBtn.innerHTML = nextIcon;
+nextBtn.title = 'Next Image';
+slideBar.appendChild(nextBtn);
+
+
+// Will change the cursor to pointer when hovering over image containing "lightbox-data" attribute.
+document.querySelectorAll('img').forEach(img => {
+    if (img.hasAttribute('lightbox-data')) {
+        img.style.cursor = 'pointer';
+        lightboxImg.style.cursor = 'zoom-in';
+    }
+});
+
+
+
+//LIGHTBOX SCRIPT STARTS
+
+// EventListener to open Lightbox Overlay on 'click' for all the IMG elements on document that contain "lightbox-data" attribute.
+// If many images on document contain same "lightbox-data" attribute value, LBO will create a gallery of all the images with same value.
+document.querySelectorAll('img').forEach(img => {
+    if (img.hasAttribute('lightbox-data')) {
+        img.addEventListener('click', function() {
+            lightBox.style.display = 'flex'
+            lightboxImg.src = img.getAttributeNode('src').value;
+            lightboxImg.alt = img.getAttributeNode('alt').value;
+            lightboxImg.getAttributeNode('lightbox-data').value = img.getAttributeNode('lightbox-data')?.value;
+            imageData.innerHTML = lightboxImg.alt;
+            if (img != lightboxImg) {       // prevents info search on click in lightbox (would build up duplicates in gallery array).
+                getSlideInfo();
+                getDownloadInfo();
+            }
+        });
+    }   
+});
+    
+        
+// Collects all the image SRC and ALT values from document on an array.
+// Function will print collected information as slide information on Lightbox overlay.
+var imageArray = []; // Creates empty array for images.
+var altArray = []; // Creates empty array for alt-data.
+function getSlideInfo() {
+    document.querySelectorAll('img').forEach(img => {
+        if (img.getAttributeNode('lightbox-data')?.value == lightboxImg.getAttributeNode('lightbox-data').value && img.getAttributeNode('lightbox-data').value != '') {
+            if (!imageArray.includes(img.getAttributeNode('src').value)) {  // Checks if imageArray already includes same img-"src". Prevents duplicates.
+                imageArray.push(img.getAttributeNode('src').value); // Adds img "src" attributes to imageArray.
+                altArray.push(img.getAttributeNode('alt').value); // Adds img "alt" attributes to altArray.
+            }
+        } 
+    });
+    var slideTotal = imageArray.length;
+    var currentSlide = imageArray.indexOf(lightboxImg.getAttributeNode('src').value); 
+    if (lightboxImg.getAttributeNode('lightbox-data').value == '') { // If there is no lightbox-data value, slide-info and browsing is disabled.
+        slideInfo.innerHTML = '';
+        previousBtn.style.display = 'none';
+        nextBtn.style.display = 'none';
+    } else {
+        slideInfo.innerHTML = ((currentSlide + 1)+ ' of ' +slideTotal); // Prints Slide information to lightbox. +1 to currentSlide get rid of the 0 index in array for slide counter.
+        previousBtn.style.display = 'block';
+        nextBtn.style.display = 'block';
+    }
+}
+
+
+// Function goes through all images on document, if image has 'download' -attribute and 'src' -attribute matches to the image presented in -
+// Lightbox Overlay, download button is allocated for that specific image inside Overlay.
+function getDownloadInfo() {
+    document.querySelectorAll('img').forEach(img => {
+        if ((img.hasAttribute('download')) && (img.getAttributeNode('src').value == lightboxImg.getAttributeNode('src').value)) {
+            imageDownload.style.display = 'flex';
+            lightboxImg.setAttribute('download', '');
+        } else {
+            imageDownload.style.display = 'none';
+        }
+    });
+}
+
+
+// Function to move on to next picture in lightbox gallery. 
+// Moves to next SRC and ALT index on array and prints the values to lightbox for corresponding image.
+nextBtn.addEventListener('click', function() {
+    lightboxImg?.removeAttribute('download', '');
+    currentSlide = imageArray.indexOf(lightboxImg.getAttributeNode('src').value);
+    if (currentSlide + 1 < imageArray.length) {
+        currentSlide++;
+        lightboxImg.src = imageArray.at(currentSlide);
+        lightboxImg.alt = altArray.at(currentSlide);
+        imageData.innerHTML = lightboxImg.alt;
+        imageArray = []; // Emptys array before new info search;
+        altArray = []; // Emptys array before new info search;
+        getSlideInfo();
+        getDownloadInfo();
+    } else {
+        lightboxImg.classList.add('end-shake');
+        getDownloadInfo();
+        setTimeout(()=> {
+            lightboxImg.classList.remove('end-shake');
+        }
+        ,500);
+    }
+});
+
+
+// Function to move to next image in gallery, but with 'keydown' event on 'ArrowRight'.
+document.addEventListener('keydown', function(e) {
+    if (e.code == 'ArrowRight') {
+    lightboxImg?.removeAttribute('download', '');
+    currentSlide = imageArray.indexOf(lightboxImg.getAttributeNode('src').value);
+        if (currentSlide + 1 < imageArray.length) {
+            currentSlide++;
+            lightboxImg.src = imageArray.at(currentSlide);
+            lightboxImg.alt = altArray.at(currentSlide);
+            imageData.innerHTML = lightboxImg.alt;
+            imageArray = []; // Emptys array before new info search;
+            altArray = []; // Emptys array before new info search;
+            getSlideInfo();
+            getDownloadInfo();
+        } else {
+            lightboxImg.classList.add('end-shake'); // This function adds shake animation when you reach end of gallery.
+            getDownloadInfo();
+            setTimeout(()=> {
+                lightboxImg.classList.remove('end-shake');
+            }
+            ,500); // This is .5s timeout that resets shake animation. This way it can shake again, if user pushes button again.
+        }
+    }
+});
+
+
+// Function to move on to previous picture in lightbox gallery. 
+// Moves to previous SRC and ALT index on array and prints the values to lightbox for corresponding image.
+previousBtn.addEventListener('click', function() {
+    lightboxImg?.removeAttribute('download', '');
+    currentSlide = imageArray.indexOf(lightboxImg.getAttributeNode('src').value);
+    if (currentSlide > 0) {
+        currentSlide--;
+        lightboxImg.src = imageArray.at(currentSlide);
+        lightboxImg.alt = altArray.at(currentSlide);
+        imageData.innerHTML = lightboxImg.alt;
+        imageArray = []; // Emptys array before new info search;
+        altArray = []; // Emptys array before new info search;
+        getSlideInfo();
+        getDownloadInfo();
+    } else {
+        lightboxImg.classList.add('end-shake'); // This function adds shake animation when you reach end of gallery.
+        getDownloadInfo();
+        setTimeout(()=> {
+            lightboxImg.classList.remove('end-shake');
+        }
+        ,500); // This is .5s timeout that resets shake animation.
+    }
+});
+
+
+// Function to move to previous image in gallery, but with 'keydown' event on 'ArrowLeft'.
+document.addEventListener('keydown', function(e) {
+    if (e.code == 'ArrowLeft') {
+        lightboxImg?.removeAttribute('download', ''); 
+        currentSlide = imageArray.indexOf(lightboxImg.getAttributeNode('src').value);
+        if (currentSlide > 0) {
+            currentSlide--;
+            lightboxImg.src = imageArray.at(currentSlide);
+            lightboxImg.alt = altArray.at(currentSlide);
+            imageData.innerHTML = lightboxImg.alt;
+            imageArray = []; // Emptys array before new info search;
+            altArray = []; // Emptys array before new info search;
+            getSlideInfo();
+            getDownloadInfo();
+        } else {
+            lightboxImg.classList.add('end-shake'); // This function adds shake animation when you reach end of gallery.
+            getDownloadInfo();
+            setTimeout(()=> {
+                lightboxImg.classList.remove('end-shake');
+            }
+            ,500); // This is .5s timeout that resets shake animation.
+        }
+    } 
+});
+
+
+// Click function to zoom in on lightbox image (one click zooms in, secont click back to normal).
+// Hides all the slide information while zoom in function is active.
+// Cursor changes between zoom-in and zoom-out when you hover on image and click to zoom.
+lightboxImg.addEventListener('click', function() {
+    if (lightboxImg.style.scale == '1.25') {
+        lightboxImg.style.scale = '1';
+        lightboxImg.style.cursor = 'zoom-in';
+        infoBar.style.display = 'flex';
+        slideBar.style.display = 'flex';
+        if (lightboxImg.getAttributeNode('lightbox-data').value != '') {
+            previousBtn.style.display = 'block';
+            nextBtn.style.display = 'block';
+        }
+    } else {
+        lightboxImg.style.scale = '1.25';
+        lightboxImg.style.cursor = 'zoom-out';
+        infoBar.style.display = 'none';
+        slideBar.style.display = 'none';
+    }
+});
+
+// Click function for 'fullScreen btn' to open Lightbox Image in fullscreen mode. Working depends on browser and device fullscreen support.
+fullScreen.addEventListener('click', function() {
+    lightboxImg.requestFullscreen();
+})
+
+
+// 'Click' eventListener for download button inside Lightbox Overlay. If download is allocated for LBO image on document,
+// download button will appear when image is presented inside LBO.
+imageDownload.addEventListener('click', function() {
+    if (lightboxImg.hasAttribute('download')) {
+        imageDownload.setAttribute('href', lightboxImg.getAttributeNode('src').value);
+        imageDownload.setAttribute('download', ''); 
+    } 
+});
+
+
+// EventListener for close button inside the lightbox overlay. Will close Lightbox.
+lightClose.addEventListener('click', function() {
+    lightboxImg?.removeAttribute('download', '');
+    lightBox.style.display = 'none';
+    lightboxImg.style.transform = 'scale(1)';
+    imageArray = [];
+    altArray = [];
+});
+
+
+// EventListener for 'BACKSPACE' key while lightbox overlay is open. Will close Lightbox.
+document.addEventListener('keydown', function(e) {
+    if (e.code == 'Backspace') {
+        lightboxImg?.removeAttribute('download', '');
+        lightBox.style.display = 'none';
+        lightboxImg.style.transform = 'scale(1)';
+        imageArray = [];
+        altArray = [];
+    }
+});
+
+
+// EventListener for 'ESCAPE' key while lightbox overlay is open. Will close Lightbox.
+document.addEventListener('keydown', function(e) {
+    if (e.code == 'Escape') {
+        lightboxImg?.removeAttribute('download', '');
+        lightBox.style.display = 'none';
+        lightboxImg.style.transform = 'scale(1)';
+        imageArray = [];
+        altArray = [];
+    }
+});
+
